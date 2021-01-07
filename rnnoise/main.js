411b11249ed65270fdd1fe4ad9eda6e8e11bd2da
miaobin <binx.miao@intel.com>
2021-01-07 15:37:02 +0800

    Fix lint error

--- rnnoise/main.js
+++ rnnoise/main.js
@@ -1,5 +1,5 @@
-import { Processer } from './processer.js';
-import { RNNoise } from './rnnoise.js';
+import {Processer} from './processer.js';
+import {RNNoise} from './rnnoise.js';
 
 let runtime = false;
 const batchSize = 1;
@@ -26,15 +26,15 @@ const recorderWorker = new Worker('./utils/recorderWorker.js');
 
 recorderWorker.postMessage({
   command: 'init',
-  config: { sampleRate: 48000, numChannels: 1 },
+  config: {sampleRate: 48000, numChannels: 1},
 });
 
-recorderWorker.onmessage = function (e) {
+recorderWorker.onmessage = function(e) {
   const blob = e.data;
   denoisedAudio.src = URL.createObjectURL(blob);
 };
 
-Module.onRuntimeInitialized = function () {
+Module.onRuntimeInitialized = function() {
   runtime = true;
   console.log('WASM Runtime Ready.');
 };
@@ -63,21 +63,21 @@ denoisedAudio.onplay = () => {
 
 async function denoise() {
   const audioData = [];
-  const audioContext = new AudioContext({ sampleRate: 48000 });
+  const audioContext = new AudioContext({sampleRate: 48000});
   const sampleRate = audioContext.sampleRate;
   const steps = 48000;
   let vadInitialHiddenStateBuffer = new Float32Array(
-    1 * batchSize * 24,
+      1 * batchSize * 24,
   ).fill(0);
   let noiseInitialHiddenStateBuffer = new Float32Array(
-    1 * batchSize * 48,
+      1 * batchSize * 48,
   ).fill(0);
   let denoiseInitialHiddenStateBuffer = new Float32Array(
-    1 * batchSize * 96,
+      1 * batchSize * 96,
   ).fill(0);
 
   if (audioContext.state != 'running') {
-    audioContext.resume().then(function () {
+    audioContext.resume().then(function() {
       console.log('audioContext resumed.');
     });
   }
@@ -103,8 +103,8 @@ async function denoise() {
     const inputBuffer = new Float32Array(features);
     start = performance.now();
     const outputs = await rnnoise.compute(
-      inputBuffer, vadInitialHiddenStateBuffer,
-      noiseInitialHiddenStateBuffer, denoiseInitialHiddenStateBuffer,
+        inputBuffer, vadInitialHiddenStateBuffer,
+        noiseInitialHiddenStateBuffer, denoiseInitialHiddenStateBuffer,
     );
     const executionTime = (performance.now() - start).toFixed(2);
     // rnnoise.dispose();
@@ -122,7 +122,7 @@ async function denoise() {
     }
 
     log(DenoiseInfo,
-      `Denoising...  ` +
+        `Denoising...  ` +
       `(${Math.ceil((i + 1) / frames * 100)}%)<br>` +
       ` - preProcessing time: <span class='text-primary'>` +
       `${preProcessingTime}</span> ms.<br>` +
@@ -159,7 +159,7 @@ $('.dropdown-item').click(async (e) => {
   } else {
     const audioUrl = getUrlById(sampleAudios, audioId);
     log(audioName,
-      audioUrl.substring(audioUrl.lastIndexOf('/') + 1), false, false);
+        audioUrl.substring(audioUrl.lastIndexOf('/') + 1), false, false);
     originalAudio.src = audioUrl;
     denoisedAudio.src = '';
     await denoise();
@@ -169,7 +169,7 @@ $('.dropdown-item').click(async (e) => {
 fileInput.addEventListener('input', (event) => {
   log(audioName, event.target.files[0].name, false, false);
   const reader = new FileReader();
-  reader.onload = async function (e) {
+  reader.onload = async function(e) {
     originalAudio.src = e.target.result;
     denoisedAudio.src = '';
     await denoise();
@@ -177,7 +177,7 @@ fileInput.addEventListener('input', (event) => {
   reader.readAsDataURL(event.target.files[0]);
 });
 
-window.onload = async function () {
+window.onload = async function() {
   log(modelInfo, `Creating RNNoise with input shape ` +
     `[${batchSize} (batch_size) x 100 (frames) x 42].`, true);
   log(modelInfo, '- Loading model...');
@@ -186,14 +186,14 @@ window.onload = async function () {
   const loadingTime = (performance.now() - start).toFixed(2);
   console.log(`loading elapsed time: ${loadingTime} ms`);
   log(modelInfo,
-    `done in <span class='text-primary'>${loadingTime}</span> ms.`, true);
+      `done in <span class='text-primary'>${loadingTime}</span> ms.`, true);
   log(modelInfo, '- Compiling model...');
   start = performance.now();
   await rnnoise.compile();
   const compilationTime = (performance.now() - start).toFixed(2);
   console.log(`compilation elapsed time: ${compilationTime} ms`);
   log(modelInfo,
-    `done in <span class='text-primary'>${compilationTime}</span> ms.`, true);
+      `done in <span class='text-primary'>${compilationTime}</span> ms.`, true);
   while (1) {
     if (runtime) {
       log(modelInfo, '- DSP library Loaded.', true);
--- rnnoise/rnnoise.js
+++ rnnoise/rnnoise.js
@@ -20,17 +20,17 @@ export class RNNoise {
 
   async buildConstantByNpy(fileName) {
     const dataTypeMap = new Map([
-      ['f2', { type: 'float16', array: Uint16Array }],
-      ['f4', { type: 'float32', array: Float32Array }],
-      ['f8', { type: 'float64', array: Float64Array }],
-      ['i1', { type: 'int8', array: Int8Array }],
-      ['i2', { type: 'int16', array: Int16Array }],
-      ['i4', { type: 'int32', array: Int32Array }],
-      ['i8', { type: 'int64', array: BigInt64Array }],
-      ['u1', { type: 'uint8', array: Uint8Array }],
-      ['u2', { type: 'uint16', array: Uint16Array }],
-      ['u4', { type: 'uint32', array: Uint32Array }],
-      ['u8', { type: 'uint64', array: BigUint64Array }],
+      ['f2', {type: 'float16', array: Uint16Array}],
+      ['f4', {type: 'float32', array: Float32Array}],
+      ['f8', {type: 'float64', array: Float64Array}],
+      ['i1', {type: 'int8', array: Int8Array}],
+      ['i2', {type: 'int16', array: Int16Array}],
+      ['i4', {type: 'int32', array: Int32Array}],
+      ['i8', {type: 'int64', array: BigInt64Array}],
+      ['u1', {type: 'uint8', array: Uint8Array}],
+      ['u2', {type: 'uint16', array: Uint16Array}],
+      ['u4', {type: 'uint32', array: Uint32Array}],
+      ['u8', {type: 'uint64', array: BigUint64Array}],
     ]);
     const response = await fetch(this.url_ + fileName);
     const buffer = await response.arrayBuffer();
@@ -46,122 +46,122 @@ export class RNNoise {
     const littleEndian = npArray.byteOrder === '<';
     for (let i = 0; i < sizeOfShape(dimensions); ++i) {
       typedArray[i] = dataView[`get` + type[0].toUpperCase() + type.substr(1)](
-        i * TypedArrayConstructor.BYTES_PER_ELEMENT, littleEndian);
+          i * TypedArrayConstructor.BYTES_PER_ELEMENT, littleEndian);
     }
-    return this.builder.constant({ type, dimensions }, typedArray);
+    return this.builder.constant({type, dimensions}, typedArray);
   }
 
   async load() {
     this.builder = nn.createModelBuilder();
 
     const inputDenseKernel0 = await this.buildConstantByNpy(
-      'input_dense_kernel_0.npy');
+        'input_dense_kernel_0.npy');
     const inputDenseBias0 = await this.buildConstantByNpy(
-      'input_dense_bias_0.npy');
+        'input_dense_bias_0.npy');
     const vadGruW = await this.buildConstantByNpy(
-      'vad_gru_W.npy');
+        'vad_gru_W.npy');
     const vadGruR = await this.buildConstantByNpy(
-      'vad_gru_R.npy');
+        'vad_gru_R.npy');
     const vadGruBData = await this.buildConstantByNpy(
-      'vad_gru_B.npy');
+        'vad_gru_B.npy');
     const noiseGruW = await this.buildConstantByNpy(
-      'noise_gru_W.npy');
+        'noise_gru_W.npy');
     const noiseGruR = await this.buildConstantByNpy(
-      'noise_gru_R.npy');
+        'noise_gru_R.npy');
     const noiseGruBData = await this.buildConstantByNpy(
-      'noise_gru_B.npy');
+        'noise_gru_B.npy');
     const denoiseGruW = await this.buildConstantByNpy(
-      'denoise_gru_W.npy');
+        'denoise_gru_W.npy');
     const denoiseGruR = await this.buildConstantByNpy(
-      'denoise_gru_R.npy');
+        'denoise_gru_R.npy');
     const denoiseGruBData = await this.buildConstantByNpy(
-      'denoise_gru_B.npy');
+        'denoise_gru_B.npy');
     const denoiseOutputKernel0 = await this.buildConstantByNpy(
-      'denoise_output_kernel_0.npy');
+        'denoise_output_kernel_0.npy');
     const denoiseOutputBias0 = await this.buildConstantByNpy(
-      'denoise_output_bias_0.npy');
+        'denoise_output_bias_0.npy');
 
     const input = this.builder.input(
-      'input', { type: 'float32', dimensions: [this.batchSize_, 100, 42] });
+        'input', {type: 'float32', dimensions: [this.batchSize_, 100, 42]});
     const inputDense0 = this.builder.matmul(input, inputDenseKernel0);
     const biasedTensorName2 = this.builder.add(inputDense0, inputDenseBias0);
     const inputDenseTanh0 = this.builder.tanh(biasedTensorName2);
     const vadGruX = this.builder.transpose(
-      inputDenseTanh0, { permutation: [1, 0, 2] });
+        inputDenseTanh0, {permutation: [1, 0, 2]});
     // hiddenSize = 24
     const vadGruB = this.builder.slice(
-      vadGruBData, [0], [3 * 24], { axes: [1] });
+        vadGruBData, [0], [3 * 24], {axes: [1]});
     const vadGruRB = this.builder.slice(
-      vadGruBData, [3 * 24], [-1], { axes: [1] });
+        vadGruBData, [3 * 24], [-1], {axes: [1]});
     const vadGruInitialH = this.builder.input(
-      'vadGruInitialH',
-      { type: 'float32', dimensions: [1, this.batchSize_, 24] });
+        'vadGruInitialH',
+        {type: 'float32', dimensions: [1, this.batchSize_, 24]});
     const [vadGruYH, vadGruY] = this.builder.gru(
-      vadGruX, vadGruW, vadGruR, this.frames_, 24, {
-      bias: vadGruB, recurrentBias: vadGruRB,
-      initialHiddenState: vadGruInitialH,
-      returnSequence: true, resetAfter: false,
-      activations: ['sigmoid', 'relu'],
-    });
+        vadGruX, vadGruW, vadGruR, this.frames_, 24, {
+          bias: vadGruB, recurrentBias: vadGruRB,
+          initialHiddenState: vadGruInitialH,
+          returnSequence: true, resetAfter: false,
+          activations: ['sigmoid', 'relu'],
+        });
     const vadGruYTransposed = this.builder.transpose(
-      vadGruY, { permutation: [2, 0, 1, 3] });
+        vadGruY, {permutation: [2, 0, 1, 3]});
     const vadGruTranspose1 = this.builder.reshape(
-      vadGruYTransposed, [-1, 100, 24]);
+        vadGruYTransposed, [-1, 100, 24]);
     const concatenate1 = this.builder.concat(
-      [inputDenseTanh0, vadGruTranspose1, input], 2);
+        [inputDenseTanh0, vadGruTranspose1, input], 2);
     const noiseGruX = this.builder.transpose(
-      concatenate1, { permutation: [1, 0, 2] });
+        concatenate1, {permutation: [1, 0, 2]});
     // hiddenSize = 48
     const noiseGruB = this.builder.slice(
-      noiseGruBData, [0], [3 * 48], { axes: [1] });
+        noiseGruBData, [0], [3 * 48], {axes: [1]});
     const noiseGruRB = this.builder.slice(
-      noiseGruBData, [3 * 48], [-1], { axes: [1] });
+        noiseGruBData, [3 * 48], [-1], {axes: [1]});
     const noiseGruInitialH = this.builder.input(
-      'noiseGruInitialH',
-      { type: 'float32', dimensions: [1, this.batchSize_, 48] });
+        'noiseGruInitialH',
+        {type: 'float32', dimensions: [1, this.batchSize_, 48]});
     const [noiseGruYH, noiseGruY] = this.builder.gru(
-      noiseGruX, noiseGruW, noiseGruR, this.frames_, 48, {
-      bias: noiseGruB, recurrentBias: noiseGruRB,
-      initialHiddenState: noiseGruInitialH,
-      returnSequence: true, resetAfter: false,
-      activations: ['sigmoid', 'relu'],
-    });
+        noiseGruX, noiseGruW, noiseGruR, this.frames_, 48, {
+          bias: noiseGruB, recurrentBias: noiseGruRB,
+          initialHiddenState: noiseGruInitialH,
+          returnSequence: true, resetAfter: false,
+          activations: ['sigmoid', 'relu'],
+        });
     const noiseGruYTransposed = this.builder.transpose(
-      noiseGruY, { permutation: [2, 0, 1, 3] });
+        noiseGruY, {permutation: [2, 0, 1, 3]});
     const noiseGruTranspose1 = this.builder.reshape(
-      noiseGruYTransposed, [-1, 100, 48]);
+        noiseGruYTransposed, [-1, 100, 48]);
     const concatenate2 = this.builder.concat(
-      [vadGruTranspose1, noiseGruTranspose1, input], 2);
+        [vadGruTranspose1, noiseGruTranspose1, input], 2);
     const denoiseGruX = this.builder.transpose(
-      concatenate2, { permutation: [1, 0, 2] });
+        concatenate2, {permutation: [1, 0, 2]});
     // hiddenSize = 96
     const denoiseGruB = this.builder.slice(
-      denoiseGruBData, [0], [3 * 96], { axes: [1] });
+        denoiseGruBData, [0], [3 * 96], {axes: [1]});
     const denoiseGruRB = this.builder.slice(
-      denoiseGruBData, [3 * 96], [-1], { axes: [1] });
+        denoiseGruBData, [3 * 96], [-1], {axes: [1]});
     const denoiseGruInitialH = this.builder.input(
-      'denoiseGruInitialH',
-      { type: 'float32', dimensions: [1, this.batchSize_, 96] });
+        'denoiseGruInitialH',
+        {type: 'float32', dimensions: [1, this.batchSize_, 96]});
     const [denoiseGruYH, denoiseGruY] = this.builder.gru(
-      denoiseGruX, denoiseGruW, denoiseGruR, this.frames_, 96, {
-      bias: denoiseGruB, recurrentBias: denoiseGruRB,
-      initialHiddenState: denoiseGruInitialH,
-      returnSequence: true, resetAfter: false,
-      activations: ['sigmoid', 'relu'],
-    });
+        denoiseGruX, denoiseGruW, denoiseGruR, this.frames_, 96, {
+          bias: denoiseGruB, recurrentBias: denoiseGruRB,
+          initialHiddenState: denoiseGruInitialH,
+          returnSequence: true, resetAfter: false,
+          activations: ['sigmoid', 'relu'],
+        });
     const denoiseGruYTransposed = this.builder.transpose(
-      denoiseGruY, { permutation: [2, 0, 1, 3] });
+        denoiseGruY, {permutation: [2, 0, 1, 3]});
     const denoiseGruTranspose1 = this.builder.reshape(
-      denoiseGruYTransposed, [-1, 100, 96]);
+        denoiseGruYTransposed, [-1, 100, 96]);
     const denoiseOutput0 = this.builder.matmul(
-      denoiseGruTranspose1, denoiseOutputKernel0);
+        denoiseGruTranspose1, denoiseOutputKernel0);
     const biasedTensorName = this.builder.add(
-      denoiseOutput0, denoiseOutputBias0);
+        denoiseOutput0, denoiseOutputBias0);
     const denoiseOutput = this.builder.sigmoid(
-      biasedTensorName);
+        biasedTensorName);
 
     this.model_ = this.builder.createModel(
-      { denoiseOutput, vadGruYH, noiseGruYH, denoiseGruYH });
+        {denoiseOutput, vadGruYH, noiseGruYH, denoiseGruYH});
   }
 
   async compile(options) {
@@ -169,13 +169,13 @@ export class RNNoise {
   }
 
   async compute(
-    inputBuffer, vadGruInitialHBuffer,
-    noiseGruInitialHBuffer, denoiseGruInitialHBuffer) {
+      inputBuffer, vadGruInitialHBuffer,
+      noiseGruInitialHBuffer, denoiseGruInitialHBuffer) {
     const inputs = {
-      input: { buffer: inputBuffer },
-      vadGruInitialH: { buffer: vadGruInitialHBuffer },
-      noiseGruInitialH: { buffer: noiseGruInitialHBuffer },
-      denoiseGruInitialH: { buffer: denoiseGruInitialHBuffer },
+      input: {buffer: inputBuffer},
+      vadGruInitialH: {buffer: vadGruInitialHBuffer},
+      noiseGruInitialH: {buffer: noiseGruInitialHBuffer},
+      denoiseGruInitialH: {buffer: denoiseGruInitialHBuffer},
     };
     const outputs = await this.compilation_.compute(inputs);
     return outputs;
